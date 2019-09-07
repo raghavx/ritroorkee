14a0ea10c56e5b015b2f8a64beb9818abfe6df52
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-07 10:20:01 +0530

    stu cons

--- src/com/ritroorkee/Student.java
+++ src/com/ritroorkee/Student.java
@@ -4,17 +4,19 @@ public class Student {
 
 	private String name;
 	private long roll;
+
 	public Student(String name, int roll) {
 		this.name = name;
 		this.roll = roll;
 	}
+
 	private void study() {
 	}
 
 	private void play() {
 	}
-	
+
 	public void declareResult() {
-		System.out.println("Result received by student");
+		System.out.println("Result received by student " + name);
 	}
 }
