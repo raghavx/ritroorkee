4441784e76d07053650f9cf5d658fcedbed8a367
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-09 10:03:47 +0530

    file and exception

--- src/com/ritroorkee/level2/ExceptionMain.java
+++ src/com/ritroorkee/level2/ExceptionMain.java
@@ -1,25 +1,26 @@
 package com.ritroorkee.level2;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class ExceptionMain {
 
 	public static void main(String[] args) {
 
 		File f = new File("training.txt");
-		if (f.exists()) {
-			System.out.println("File exists");
-		} else {
-			System.out.println("Doesn't exits");
-			try {
-				f.createNewFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(f);
+			writer.println("This is a text");
+		} catch (FileNotFoundException e) {
+
+		} finally {
+			if (writer != null) {
+				writer.close();
 			}
 		}
-
+		System.out.println(f.getAbsolutePath());
 	}
 
 }
